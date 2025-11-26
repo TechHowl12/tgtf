2e426566aaf5f61f874456a9ad63155bb6137583
codewithafzall <afzalshaikh19992@gmail.com>
2025-11-26 15:02:12 +0530

    fixed highlight section

--- src/app/components/Home/Highlights.jsx
+++ src/app/components/Home/Highlights.jsx
@@ -247,7 +247,7 @@ const Highlights = () => {
                                 alt={ev.title}
                                 className="rounded-xl w-full xl:max-w-[665px] h-[550px] object-cover"
                             />
-                            <div className="bg-darkgrey w-full xl:max-w-[665px] mt-6 curve py-6 px-8 flex justify-between gap-x-4 h-36 items-start">
+                            <div className="bg-darkgrey w-full xl:max-w-[665px] mt-6 curve py-6 px-6 flex justify-between gap-x-4 h-36 items-start">
                                 <div className="w-2/6">
                                     <small className="text-blue font-semibold text-lg block leading-5">
                                         {ev.title}
