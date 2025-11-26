a5a247b58d5b3a2b27ba178b67c525e0c42e17bd
codewithafzall <afzalshaikh19992@gmail.com>
2025-11-26 23:50:39 +0530

    video added

--- src/app/components/Home/PartnersCarousel.jsx
+++ src/app/components/Home/PartnersCarousel.jsx
@@ -1,18 +1,17 @@
 "use client";
 import React, { useRef, useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import ponant from "../../images/ponant.webp";
-import saudi from "../../images/saudi.webp";
-import qatar from "../../images/qatar.webp";
-import seychelles from "../../images/seychelles.webp";
-import utah from "../../images/utah.webp";
 import clubmed from "../../images/club-med.webp";
+import immersive from "../../images/immersive.png";
+import thailand from "../../images/thailand.png";
+import kohventure from "../../images/kohventure.png";
+import ana from "../../images/ana.png";
+import latitude from "../../images/latitude.png";
 import arrow from "../../images/next-arrow.png";
+import partnersDesktop from "../../images/partners-desktop.png"; // renamed to avoid name clash
 import "swiper/css";
-import "swiper/css/autoplay";
 import "swiper/css/navigation";
-import { Autoplay, Navigation } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 import Image from "next/image";
 
 const PartnersCarousel = () => {
@@ -23,31 +22,41 @@ const PartnersCarousel = () => {
   const [isEnd, setIsEnd] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
-  // detect mobile (<= 767px) and update on resize
   useEffect(() => {
     if (typeof window === "undefined") return;
     const mq = window.matchMedia("(max-width: 767px)");
-    const handler = (e) => setIsMobile(e.matches);
+
+    const handler = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    // initial value
     setIsMobile(mq.matches);
+
     if (mq.addEventListener) mq.addEventListener("change", handler);
     else mq.addListener(handler);
+
     return () => {
       if (mq.removeEventListener) mq.removeEventListener("change", handler);
       else mq.removeListener(handler);
     };
   }, []);
 
-  const partners = [saudi, ponant, qatar];
-  const sponsors = [utah, clubmed, seychelles];
+  // category groups
+  const destinationPartners = [thailand, ana];
+  const featuredPartners = [clubmed];
+  const partnerLogos = [latitude, kohventure, immersive];
 
+  // mobile: one logo per slide with title
   const mobileSlides = [
-    ...partners.map((logo) => ({ title: "Partners", logo })),
-    ...sponsors.map((logo) => ({ title: "Sponsor", logo })),
+    ...destinationPartners.map((logo) => ({ title: "Destination partners", logo })),
+    ...featuredPartners.map((logo) => ({ title: "Featured partners", logo })),
+    ...partnerLogos.map((logo) => ({ title: "Partners", logo })),
   ];
 
   return (
-    <div className="w-full py-20 px-16 sm:px-20 bg-grey relative">
-      {/* Prev */}
+    <div className="w-full pt-20 pb-10 lg:py-0 lg:pt-16 lg:pb-6 md:px-16 lg:px-10 sm:px-20 bg-grey relative">
+      {/* Prev (only for swiper, and only visible <1024px because of lg:hidden below) */}
       <button
         ref={prevRef}
         disabled={isBeginning}
@@ -74,89 +83,114 @@ const PartnersCarousel = () => {
       <h1 className="text-pink absolute top-6 xl:top-5 text-center left-1/2 -translate-x-1/2 z-20 whitespace-nowrap">
         Featured <span className="font-bold">partners</span>
       </h1>
-      {/* <a
-        className="absolute bottom-4 sm:bottom-8 xl:bottom-7 left-1/2 -translate-x-1/2 bg-pink text-white rounded-xs py-0 uppercase px-2 z-20"
-        href="/"
-      >
-        see more
-      </a> */}
 
-      <Swiper
-        modules={[Navigation]}
-        loop={false}
-        speed={600}
-        slidesPerView={1}
-        spaceBetween={32}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-        breakpoints={{
-          768: { slidesPerView: 1 },
-          1024: { slidesPerView: 2 },
-          1280: { slidesPerView: 2 },
-        }}
-        onBeforeInit={(swiper) => {
-          swiper.params.navigation.prevEl = prevRef.current;
-          swiper.params.navigation.nextEl = nextRef.current;
-        }}
-        onSwiper={(swiper) => {
-          swiper.params.navigation.prevEl = prevRef.current;
-          swiper.params.navigation.nextEl = nextRef.current;
-          swiper.navigation.init();
-          swiper.navigation.update();
-          setIsBeginning(swiper.isBeginning);
-          setIsEnd(swiper.isEnd);
-        }}
-        onSlideChange={(swiper) => {
-          setIsBeginning(swiper.isBeginning);
-          setIsEnd(swiper.isEnd);
-        }}
-        className="w-full"
-      >
-        {isMobile ? (
-          mobileSlides.map(({ title, logo }, i) => (
-            <SwiperSlide key={`${title}-${i}`}>
-              <div className="relative bg-white rounded-lg w-full h-[210px]">
-                <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm">
-                  {title}
+      {/* SLIDER: visible only below 1024px */}
+      <div className="lg:hidden">
+        <Swiper
+          modules={[Navigation]}
+          loop={false}
+          speed={600}
+          slidesPerView={1}
+          spaceBetween={32}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          breakpoints={{
+            768: { slidesPerView: 1 },
+            1024: { slidesPerView: 2 },
+            1280: { slidesPerView: 2 },
+          }}
+          onBeforeInit={(swiper) => {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+          }}
+          onSwiper={(swiper) => {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+            setIsBeginning(swiper.isBeginning);
+            setIsEnd(swiper.isEnd);
+          }}
+          onSlideChange={(swiper) => {
+            setIsBeginning(swiper.isBeginning);
+            setIsEnd(swiper.isEnd);
+          }}
+          className="w-full"
+        >
+          {isMobile ? (
+            // MOBILE: one logo per slide
+            mobileSlides.map(({ title, logo }, i) => (
+              <SwiperSlide key={`${title}-${i}`}>
+                <div className="relative bg-white rounded-lg w-full h-[210px]">
+                  <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm whitespace-nowrap">
+                    {title}
+                  </div>
+                  <div className="h-full w-full flex items-center justify-center px-4 pt-6">
+                    <Image width={200} height={80} src={logo} alt={`${title} Logo`} />
+                  </div>
                 </div>
-                <div className="h-full w-full flex items-center justify-center px-4 pt-6">
-                  <Image src={logo} alt={`${title} Logo`} />
-                </div>
-              </div>
-            </SwiperSlide>
-          ))
-        ) : (
-          <>
-            <SwiperSlide>
-              <div className="relative bg-white rounded-lg w-full h-[210px] lg:h-40">
-                <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm">
-                  Partners
-                </div>
-                <div className="h-full w-full flex items-center justify-center gap-10 xl:gap-20 px-4 pt-6">
-                  <Image src={saudi} alt="Saudi Logo" className="" />
-                  <Image src={ponant} alt="Ponant Logo" className="" />
-                  <Image src={qatar} alt="Qatar Logo" className="" />
-                </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
+            ))
+          ) : (
+            <>
+              {/* 768px–1023px: three category slides */}
 
-            <SwiperSlide>
-              <div className="relative bg-white rounded-lg w-full h-[210px] lg:h-40">
-                <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm">
-                  Sponsors
+              {/* Destination partners */}
+              <SwiperSlide>
+                <div className="relative bg-white rounded-lg w-full h-[210px] lg:h-40">
+                  <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm">
+                    Destination partners
+                  </div>
+                  <div className="h-full w-full flex items-center justify-center gap-10 xl:gap-20 px-4 pt-6">
+                    {destinationPartners.map((logo, i) => (
+                      <Image width={200} height={50} key={i} src={logo} alt="Destination Partner Logo" />
+                    ))}
+                  </div>
                 </div>
-                <div className="h-full w-full flex items-center justify-center gap-10 xl:gap-20 px-4 pt-6">
-                  <Image src={utah} alt="Utah Logo" className="" />
-                  <Image src={clubmed} alt="Club Med Logo" className="" />
-                  <Image src={seychelles} alt="Seychelles Logo" className="" />
+              </SwiperSlide>
+
+              {/* Featured partners */}
+              <SwiperSlide>
+                <div className="relative bg-white rounded-lg w-full h-[210px] lg:h-40">
+                  <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm">
+                    Featured partners
+                  </div>
+                  <div className="h-full w-full flex items-center justify-center gap-10 xl:gap-20 px-4 pt-6">
+                    {featuredPartners.map((logo, i) => (
+                      <Image width={250} height={50} key={i} src={logo} alt="Featured Partner Logo" />
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          </>
-        )}
-      </Swiper>
+              </SwiperSlide>
+
+              {/* Partners */}
+              <SwiperSlide>
+                <div className="relative bg-white rounded-lg w-full h-[210px] lg:h-40">
+                  <div className="absolute top-3 left-1/2 -translate-x-1/2 text-pink text-sm">
+                    Partners
+                  </div>
+                  <div className="h-full w-full flex items-center justify-center gap-10 xl:gap-20 px-4 pt-6">
+                    {partnerLogos.map((logo, i) => (
+                      <Image width={150} height={50} key={i} src={logo} alt="Partner Logo" />
+                    ))}
+                  </div>
+                </div>
+              </SwiperSlide>
+            </>
+          )}
+        </Swiper>
+      </div>
+
+      {/* STATIC IMAGE: only ≥1024px */}
+      <Image
+        width={1280}
+        height={100}
+        src={partnersDesktop}
+        alt="Partners strip"
+        className="w-full hidden lg:block"
+      />
     </div>
   );
 };
