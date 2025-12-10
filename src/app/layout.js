d0cfe7efdc94408c4b7faf9ed25e61b729242a2b
codewithafzall <afzalshaikh19992@gmail.com>
2025-12-10 18:37:52 +0530

    google analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -72,6 +72,22 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
 
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JJZKYYZ79T"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JJZKYYZ79T');
+        `}
+        </Script>
+
+
         {/* Meta Pixel Code */}
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
